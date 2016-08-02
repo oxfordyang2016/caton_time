f87d8000a2e47841c03d46b220f197dd6ccbbb25
yangming <yang756260386@gmail.com>
2016-08-02 17:08:43 +0800

    al

--- json_client_server_allgo/server.go
+++ json_client_server_allgo/server.go
@@ -47,7 +47,7 @@ func main() {
 		if err != nil {
 			continue
 		}
-		fmt.Println(string(ioutil.ReadAll(conn))
+		fmt.Println(string(ioutil.ReadAll(conn)))
 
 		encoder := json.NewEncoder(conn)
 		decoder := json.NewDecoder(conn)
