87423800694dfa95de3ef5e51da021324a89faa6
yangming <yang756260386@gmail.com>
2016-08-29 18:17:48 +0800

    allok

--- hodgepodge/httpbodypaser.go
+++ hodgepodge/httpbodypaser.go
@@ -23,6 +23,7 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"reflect"
 )
 
 type test_struct struct {
@@ -31,12 +32,20 @@ type test_struct struct {
 
 func test(rw http.ResponseWriter, req *http.Request) {
 	body, err := ioutil.ReadAll(req.Body)
+	
+	fmt.Println(reflect.TypeOf(body))
+	fmt.Println(reflect.TypeOf(req.Body))
+	fmt.Println(reflect.TypeOf(string(body))
+	fmt.Println(req.Body)
+	fmt.Println(body) //if only here,it will occur  byte
+	fmt.Println(string(body))
 	if err != nil {
 		//panic()
 		fmt.Println("erorr")
 	}
+
 	log.Println(string(body))
-	fmt.Println(string(body))
+
 	var t test_struct
 	err = json.Unmarshal(body, &t)
 	if err != nil {
