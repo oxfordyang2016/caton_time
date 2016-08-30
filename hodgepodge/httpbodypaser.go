01fe2b7624beed5d023e53871330c043d6e2db97
yangming <yang756260386@gmail.com>
2016-08-30 16:40:39 +0800

    allok

--- hodgepodge/httpbodypaser.go
+++ hodgepodge/httpbodypaser.go
@@ -1,3 +1,5 @@
+package main
+
 /*
 
 
@@ -14,76 +16,117 @@ dskklsflk
 
 
 */
-
-package main
-
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/dgrijalva/jwt-go"
+	"io"
+
 	"io/ioutil"
 	"log"
 	"net/http"
 	"reflect"
+	"strings"
+	//"time"
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type test_struct struct {
 	Test string
 }
+type Response struct {
+	Token  string
+	Status int
+}
 
 func test(rw http.ResponseWriter, req *http.Request) {
-//-----------------------http body deal--------------------------------
-	body, err := ioutil.ReadAll(req.Body)
+	//------------------------------------------database connettion----------------------------
+	db, err := sql.Open("mysql",
+		"root:123456@tcp(192.168.0.81:3306)/hello") //first configure a database
+	if err != nil {
+		fmt.Println("database error")
+		log.Fatal(err)
+	}
+	fmt.Println("database connect success")
+	defer db.Close()
+	//---------------------------------------------------------------------------------------------------
+	body, _ := ioutil.ReadAll(req.Body)
+	//header, _ := ioutil.ReadAll(req.Header)
 	fmt.Println(reflect.TypeOf(body))
 	fmt.Println(reflect.TypeOf(req.Body))
-	fmt.Println(reflect.TypeOf(string(body))
+	fmt.Println(reflect.TypeOf(string(body)))
 	fmt.Println(req.Body)
 	fmt.Println(body) //if only here,it will occur  byte
 	fmt.Println(string(body))
-	if err != nil {
-		//panic()
-		fmt.Println("erorr")
+	//fmt.Println(string(header))
+	fmt.Println(formatRequest(req))
+	//--------------http response------------------------------------->
+	// Create a new token object, specifying signing method and the claims
+	// you would like it to contain.
+	mySigningKey := "caton"
+	to, _ := ExampleNew([]byte(mySigningKey))
+	fmt.Println(to)
+	fmt.Println(reflect.TypeOf(to))
+	rep := &Response{Token: to, Status: 1}
+	b, _ := json.Marshal(rep)
+	io.WriteString(rw, string(b)) //write to body
+
+	//--------------------http server --------------------------
+	/*http server
+
+	package main
+
+	import (
+		"io"
+		"net/http"
+	)
+
+	func hello(w http.ResponseWriter, r *http.Request) {
+		io.WriteString(w, "Hello world!")
 	}
 
-	log.Println(string(body))
-
-	var t test_struct
-	err = json.Unmarshal(body, &t)
-	if err != nil {
-		//panic()
-		fmt.Println("error")
+	func main() {
+		http.HandleFunc("/", hello)
+		http.ListenAndServe(":8000", nil)
 	}
-	log.Println(t.Test)
 
-	fmt.Println(formatRequest(req))//request header deal
+	*/
+	//--------------------------------------------------------------
+	//----------------------------------------------struct to json-----------------------------
+	/*
+	   package main
 
-/*http server
+	   import (
+	       "fmt"
+	       "encoding/json"
+	   )
 
-package main
+	   type User struct {
+	       Name string
+	   }
 
-import (
-	"io"
-	"net/http"
-)
+	   func main() {
+	       user := &User{Name: "Frank"}
+	       b, err := json.Marshal(user)
+	       if err != nil {
+	           fmt.Println(err)
+	           return
+	       }
+	       fmt.Println(string(b))
+	   }
+	*/
 
-func hello(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "Hello world!")
+	//output:{"Name":"Frank"}
 }
 
 func main() {
-	http.HandleFunc("/", hello)
-	http.ListenAndServe(":8000", nil)
-}
-
-*/
-
-
-}
-
-func main() {
-	http.HandleFunc("/test", test)
+	http.HandleFunc("/api/v1/login", test)
 	log.Fatal(http.ListenAndServe(":8082", nil))
+
 }
-//=---------------------------------------http header deal-----------------------------------------
+
+// formatRequest generates ascii representation of a request
 func formatRequest(r *http.Request) string {
 	// Create return string
 	var request []string
@@ -110,6 +153,7 @@ func formatRequest(r *http.Request) string {
 	// Return the request as a string
 	return strings.Join(request, "\n")
 }
+
 //------------------------jwt token create------------- create jwt token-------------------
 func ExampleNew(mySigningKey []byte) (string, error) {
 	// Create the token
@@ -121,4 +165,3 @@ func ExampleNew(mySigningKey []byte) (string, error) {
 	tokenString, err := token.SignedString(mySigningKey)
 	return tokenString, err
 }
-//-----------------------------------------------------------------------------------------
