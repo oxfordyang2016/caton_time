f2bcfbf02b10437a5394c17cad68f62029766030
yangming <yang756260386@gmail.com>
2016-12-09 10:44:52 +0800

    allok

--- hodgepodge/threadtutorialcode.py
+++ hodgepodge/threadtutorialcode.py
@@ -1,12 +1,13 @@
 import time
+import urllib2
 #http://agiliq.com/blog/2013/09/understanding-threads-in-python/
 def get_responses():
     urls = ['http://www.google.com', 'http://www.amazon.com', 'http://www.ebay.com', 'http://www.alibaba.com', 'http://www.reddit.com']
     start = time.time()
     for url in urls:
-        print url
+        print (url)
         resp = urllib2.urlopen(url)
-        print resp.getcode()
-    print "Elapsed time: %s" % (time.time()-start)
+        print (resp.getcode())
+    print ("Elapsed time: %s" % (time.time()-start))
 
-get_response()
+get_responses()
