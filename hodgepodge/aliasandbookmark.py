1506449ca7dcb5aad6bba1f31e925a226b591ed7
yangming <yang756260386@gmail.com>
2017-01-16 10:51:21 +0800

    allok

---
+++ hodgepodge/aliasandbookmark.py
@@ -0,0 +1,22 @@
+'''
+#ipython  alias and bookmarks
+#alias
+%alias <name>  [command]
+%alias  listdir  ls -al
+
+input ---
+%listdir
+
+'''
+
+'''
+%bookmark <name>  [directory]
+%bookmark  py /opt/cydex/ 
+command line
+%cd py
+% bookmark -l :list all bookmark
+%bookmark -d  :<name>:remove bookmark <name>
+%bookmark -r :remove all bookmarks
+
+
+'''
